e41c49a6fdbbf37de8c822aa8a445b93c3cc7266
KrzysDev <krzysiekk.sokolowski@gmail.com>
2026-07-15 21:02:53 +0200

    feat: added edit task endpoint

--- my_work/main.py
+++ my_work/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from schemas.schemas import Task
 
-app = FastAPI()
 
-tasks: list[Task] = []
+all_tasks = []
+
+app = FastAPI()
 
 @app.get("/")
 def information():
@@ -15,22 +16,31 @@ def health():
 
 @app.get("/tasks")
 def tasks():
-    return tasks
+    return all_tasks
 
-@app.get("/tasks/{id}")
+@app.get("/get_task/{id}")
 def get_task(id):
-    for task in tasks:
+    for task in all_tasks:
         if task.id == id:
             return task
 
 
-@app.post("/tasks")
-def add_task():
-    pass
+@app.post("/add_task")
+def add_task(task: Task):
+    all_tasks.append(task)
+    return {"task appended status" : "success"}
 
-@app.post("/tasks/{id}")
-def edit_task(id):
-    pass
+
+@app.post("/edit_task/{id}")
+def edit_task(id, task: Task):
+    for i in range(len(all_tasks)):
+        print(id, all_tasks[i].id)
+        if int(id) == int(all_tasks[i].id):
+            all_tasks[i] = task
+            return {"edited task status" : "success"}
+        
+            
+            
 
 @app.delete("/tasks/{id}")
 def delete_task(id):
