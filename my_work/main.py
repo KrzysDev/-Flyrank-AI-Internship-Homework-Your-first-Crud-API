d72c02b97b08d88d07bc1ba9b2b1a116fc480b85
KrzysDev <krzysiekk.sokolowski@gmail.com>
2026-07-16 21:59:35 +0200

    changed input in POST task endpoint (its just title now not the whole task. I misunderstood assignment

--- my_work/main.py
+++ my_work/main.py
@@ -6,41 +6,54 @@ all_tasks = []
 
 app = FastAPI()
 
+
 @app.get("/")
 def information():
-    return {"something" : "here"}
+    return {"something": "here"}
+
 
 @app.get("/health")
 def health():
-    return {"status" : "ok"}
+    return {"status": "ok"}
+
 
 @app.get("/tasks")
 def tasks():
     return all_tasks
 
-@app.get("/get_task/{id}")
+
+@app.get("/tasks/{id}")
 def get_task(id):
     for task in all_tasks:
         if task.id == id:
             return task
 
 
-@app.post("/add_task")
-def add_task(task: Task):
+@app.post("/tasks")
+def add_task(title: str):
+    task = Task()
+
+    task.title = title
+
+    last_id = tasks[len(tasks) - 1].id
+    
+    task.id = last_id + 1
+
+    task.done = False
+
     all_tasks.append(task)
-    return {"task appended status" : "success"}
+    
+    return {"task appended status": "success"}
 
 
-@app.post("/edit_task/{id}")
+@app.put("/tasks/{id}")
 def edit_task(id, task: Task):
     for i in range(len(all_tasks)):
         print(id, all_tasks[i].id)
         if int(id) == int(all_tasks[i].id):
             all_tasks[i] = task
-            return {"edited task status" : "success"}
-        
-            
-            
+            return {"edited task status": "success"}
+
 
 @app.delete("/tasks/{id}")
 def delete_task(id):
